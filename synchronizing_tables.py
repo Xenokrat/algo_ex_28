ada2708c2cc32e582cc01bd461c95a6bede18657
Mantis <barabanovpv95@gmail.com>
2022-08-07 11:23:42 +0300

    5. Solution for SynchronizingTables added

--- synchronizing_tables.py
+++ synchronizing_tables.py
@@ -1,6 +1,8 @@
 def SynchronizingTables(N: int, ids: list[int], salary: list[int]) -> list[int]:
-    salary.sort()
+    salary.sort()  # sorting list of salaries
     zipped_ids_salary = list(zip(ids, salary))
+
+    # sort list of pairs ids <--> salary by ids order
     sorted_tuples = sorted(zipped_ids_salary, key=lambda x: x[0])
-    res = [x for _, x in sorted_tuples]
+    res = [x for _, x in sorted_tuples]  # extract salaries from list of tuples
     return res
