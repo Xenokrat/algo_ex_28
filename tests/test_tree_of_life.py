351fdb9ec6856415870507fdba8dd83deb349cea
Xenokrat <ksenokrat@gmail.com>
2022-08-20 14:46:52 +0300

    feat 23: add new methods to TreeOfLife problem

--- tests/test_tree_of_life.py
+++ tests/test_tree_of_life.py
@@ -1,11 +1,31 @@
 import unittest
-from tree_of_life import TreeOfLife
+from tree_of_life import *
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        res = TreeOfLife(3, 4, 12, ['.+..', '..+.', '.+..'])
+    # def test_something(self):
+    #     res = TreeOfLife(3, 4, 5, ['.+..', '..+.', '.+..'])
+    #     self.assertListEqual(res, ['.+..', '..+.', '.+..'])
+
+    def test_parse_string(self):
+        res = parse_string(['.+..', '..+.', '.+..'])
+        self.assertListEqual(res, [[0, 1, 0, 0],
+                                   [0, 0, 1, 0],
+                                   [0, 1, 0, 0]])
+
+    def test_parse_string_back(self):
+        res = parse_string_back([[0, 1, 0, 0],
+                                 [0, 0, 1, 0],
+                                 [0, 1, 0, 0]])
         self.assertListEqual(res, ['.+..', '..+.', '.+..'])
+
+    def test_even_year(self):
+        res = even_year([[0, 1, 0, 0],
+                         [0, 0, 1, 0],
+                         [0, 1, 0, 0]], 3, 4)
+        self.assertListEqual(res, [[1, 2, 1, 1],
+                                   [1, 1, 2, 1],
+                                   [1, 2, 1, 1]])
 
 
 if __name__ == '__main__':
--- tree_of_life.py
+++ tree_of_life.py
@@ -1,6 +1,62 @@
 def TreeOfLife(h: int, w: int, n: int, tree: list[str]) -> list[str]:
-    pass
+    # main loop
+
+    parsed_tree = parse_string(tree)
+    for year in range(1, n):
+        if year % 2 == 1:
+            pass
 
 
-def parse_string(tree: list[str]) -> list[int]:
-    pass
+def even_year(parsed_tree: list[list[int]], h: int, w: int) -> list[list[int]]:
+    # create new branches, increase age of existing branches
+    for row in range(h):
+        for col in range(w):
+            parsed_tree[row][col] += 1
+    return parsed_tree
+
+
+def odd_year(parsed_tree: list[list[int]], h: int, w: int) -> list[list[int]]:
+    # branches become older
+    new_parsed_tree = even_year(parsed_tree)
+
+    branches_to_del = set()
+    for row in range(h):
+        for col in range(w):
+            if new_parsed_tree[row][col] > 2:
+                branches_to_del.add((row, col))
+
+    for branch in branches_to_del.copy():
+        # add upper
+        if branch[0] > 0:
+            branches_to_del.add((branch[0] - 1, branch[1]))
+
+        # add bottom
+        if branch[0] < h - 1:
+            branches_to_del.add((branch[0] + 1, branch[1]))
+
+        # add left
+        if branch[1] > 0:
+            branches_to_del.add((branch[0], branch[1] - 1))
+
+        # add right
+        if branch[1] < w - 1:
+            branches_to_del.add((branch[0], branch[1] + 1))
+
+
+def parse_string(tree: list[str]) -> list[list[int]]:
+    res = []
+    for symbol_line in tree:
+        int_line = [0 if symbol == '.' else 1 for symbol in symbol_line]
+        res.append(int_line)
+
+    return res
+
+
+def parse_string_back(tree: list[list[int]]) -> list[int]:
+    res = []
+    for int_line in tree:
+        symbols_line = ['.' if num == 0 else '+' for num in int_line]
+        string_line = ''.join(symbols_line)
+        res.append(string_line)
+
+    return res
