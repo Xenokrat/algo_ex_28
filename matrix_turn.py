e47d77222345b825470b607ae7e675498d52d1c4
Xenokrat <ksenokrat@gmail.com>
2022-08-21 12:03:13 +0300

    feat 24: add MatrixTurn solution and tests

--- matrix_turn.py
+++ matrix_turn.py
@@ -7,33 +7,73 @@ def MatrixTurn(
         cols: int,
         t: int) -> None:
 
-    assert min(rows, cols) % 2 == 0
-
     matrix_copy = [list(x) for x in matrix]
-    circle_indexes = make_circle_indexes(rows, cols)
-    rotated_indexes = shift_circle(circle_indexes, t)
-    rotate_circle(matrix_copy)
+
+    # assert min(rows, cols) % 2 == 0
+    circles_count = min(rows, cols) // 2
+
+    circles_list = []
+    for shift in range(circles_count):
+        new_rows = rows - shift * 2
+        new_cols = cols - shift * 2
+        circle = make_circle_indexes(matrix_copy,
+                                     new_rows, new_cols, shift)
+        rotated_circle = rotate_circle(circle, t)
+        circles_list.extend(
+            rotated_circle
+        )
+
+    # write things back in empty matrix
+    for ind_value in circles_list:
+        row_ = ind_value[0][0]
+        col_ = ind_value[0][1]
+        val = ind_value[1]
+        matrix_copy[row_][col_] = val
+
+    matrix_copy_str = [''.join(i) for i in matrix_copy]
+
+    for row in range(rows):
+        matrix[row] = matrix_copy_str[row]
 
 
-def make_circle_indexes(rows: int, cols: int) -> list[tuple]:
-    upper = [(0, col) for col in range(cols)]
-    right = [(row, cols - 1) for row in range(1, rows)]
-    bottom = [(rows - 1, col) for col in range(cols - 2, -1, -1)]
-    left = [(row, 0) for row in range(rows - 2, 0, -1)]
+def make_circle_indexes(matrix: list[list[str]],
+                        rows: int, cols: int, shift: int) -> list[list]:
+    # ((row, column), value)
+    upper = [
+        [(0 + shift, col + shift),
+         matrix[0 + shift][col + shift]]
+        for col in range(cols)
+    ]
+
+    right = [
+        [(row + shift, cols - 1 + shift),
+         matrix[row + shift][cols - 1 + shift]]
+        for row in range(1, rows)
+    ]
+
+    bottom = [
+        [(rows - 1 + shift, col + shift),
+         matrix[rows - 1 + shift][col + shift]]
+        for col in range(cols - 2, -1, -1)
+    ]
+
+    left = [
+        [(row + shift, 0 + shift),
+         matrix[row + shift][0 + shift]]
+        for row in range(rows - 2, 0, -1)
+    ]
+
     circle = upper + right + bottom + left
+
     return circle
 
 
-def shift_circle(circle: list[tuple], t: int) -> list[tuple]:
-    deque_circle = deque(circle)
+def rotate_circle(circle: list[list], t: int) -> list[list]:
+    new_circle = circle.copy()
+    value_circle = [val[1] for val in new_circle]
+    deque_circle = deque(value_circle)
     deque_circle.rotate(t)
-    return list(deque_circle)
+    for ind, val in enumerate(new_circle):
+        new_circle[ind][1] = deque_circle[ind]
 
-
-def rotate_circle(matrix: list[str],
-                  rotated_indexes: list[tuple],
-                  rows: int,
-                  cols: int) -> None:
-    empty_matrix = [[0] * cols for _ in range(rows)]
-    for pos in rotated_indexes:
-        pass
+    return new_circle
--- tests/test_matrix_turn.py
+++ tests/test_matrix_turn.py
@@ -3,24 +3,39 @@ from matrix_turn import *
 
 
 class MyTestCase(unittest.TestCase):
-    # def test_matrix_turn(self):
-    #     matrix = ["123456", "234567", "345678", "456789"]
-    #     MatrixTurn(matrix, 4, 6, 3)
-    #     self.assertListEqual(matrix, ['432123',
-    #                                   '565434',
-    #                                   '676754',
-    #                                   '789876'])
+    def test_matrix_turn(self):
+        matrix = ["123456", "234567", "345678", "456789"]
+        MatrixTurn(matrix, 4, 6, 3)
+        self.assertListEqual(matrix, ['432123',
+                                      '565434',
+                                      '676545',
+                                      '789876'])
 
     def test_make_circle_indexes(self):
-        res = make_circle_indexes(3, 4)
-        self.assertListEqual(res, [(0, 0), (0, 1), (0, 2), (0, 3),
-                                   (1, 3), (2, 3),
-                                   (2, 2), (2, 1), (2, 0),
-                                   (1, 0)])
+        matrix = ['1234',
+                  '2345']
+        matrix_copy = [list(i) for i in matrix]
+        res = make_circle_indexes(matrix_copy, 2, 4, 0)
+        self.assertListEqual(
+            res,
+            [[(0, 0), '1'], [(0, 1), '2'], [(0, 2), '3'], [(0, 3), '4'],
+             [(1, 3), '5'], [(1, 2), '4'], [(1, 1), '3'], [(1, 0), '2']]
+        )
 
-    def test_shift_circle(self):
-        res = shift_circle([(0, 0), (0, 1), (0, 2), (0, 3)], 2)
-        self.assertListEqual(res, [(0, 2), (0, 3), (0, 0), (0, 1)])
+    def test_rotate_circle(self):
+        res = rotate_circle(
+            [[(0, 0), '1'], [(0, 1), '2'], [(0, 2), '3'], [(0, 3), '4'],
+             [(1, 3), '5'], [(1, 2), '4'], [(1, 1), '3'], [(1, 0), '2']],
+            2
+        )
+        template = [
+            [(0, 0), '3'], [(0, 1), '2'], [(0, 2), '1'], [(0, 3), '2'],
+            [(1, 3), '3'], [(1, 2), '4'], [(1, 1), '5'], [(1, 0), '4']
+        ]
+        self.assertListEqual(
+            res,
+            template
+        )
 
 
 if __name__ == '__main__':
