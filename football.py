b3d9e6f5836643b3437926b363c6149c09dba601
Mantis <barabanovpv95@gmail.com>
2022-08-22 22:57:02 +0300

    feat 27: add Football solution and tests

--- football.py
+++ football.py
@@ -1,2 +1,61 @@
 def Football(f: list[int], n: int) -> bool:
-    pass
+    can_reverse = can_reverse_sort(f, n)
+    can_swap = can_swap_sort(f, n)
+    return any((can_reverse, can_swap))
+
+
+# find if we can sort by reversing subarray
+def can_reverse_sort(f: list[int], n: int) -> bool:
+    subarray_start = None
+
+    # find where reversed subarray starts
+    for ind in range(n - 1):
+        if f[ind] > f[ind + 1]:
+            subarray_start = ind
+            break
+
+    # if already sorted
+    if subarray_start is None:
+        return True
+
+    # find reversed subarray endpoint
+    ind = subarray_start
+    while f[ind] > f[ind + 1] and ind < n:
+        ind += 1
+        if ind == n - 1:
+            break
+
+    subarray_end = ind
+
+    # check if list will be sorted after reversing subarray
+    # all conditions should be true
+    if subarray_start > 0:
+        condition1 = f[subarray_start - 1] < f[subarray_end]
+    else:
+        condition1 = True
+
+    if subarray_end < n - 1:
+        condition2 = f[subarray_start] < f[subarray_end + 1]
+    else:
+        condition2 = True
+
+    # if there is other inversed numbers
+    while subarray_end < n - 1:
+        if f[subarray_end] > f[subarray_end + 1]:
+            return False
+        subarray_end += 1
+
+    return all([condition1, condition2])
+
+
+def can_swap_sort(f: list[int], n: int) -> bool:
+    f_copy = f.copy()
+    f_copy.sort()
+
+    counter = 0
+    for ind in range(n):
+        if f[ind] != f_copy[ind]:
+            counter += 1
+
+    res = (counter == 0) or (counter == 2)
+    return res
--- tests/test_football.py
+++ tests/test_football.py
@@ -16,6 +16,39 @@ class MyTestCase(unittest.TestCase):
 
         self.assertFalse(res4)
 
+    def test_can_reverse_sort(self):
+        res1 = can_reverse_sort([10, 7, 5, 3, 9], 5)
+        res2 = can_reverse_sort([1, 7, 5, 3, 9, 10, 11], 7)
+        res3 = can_reverse_sort([1, 2, 3], 3)
+        res4 = can_reverse_sort([3, 2, 1], 3)
+        res5 = can_reverse_sort([9, 5, 3, 7, 1], 5)
+
+        self.assertTrue(res2)
+        self.assertTrue(res3)
+        self.assertTrue(res4)
+
+        self.assertFalse(res1)
+        self.assertFalse(res5)
+
+    def test_can_swap_sort(self):
+        res1 = can_swap_sort([1, 3, 2], 3)
+        res2 = can_swap_sort([3, 2, 1], 3)
+        res3 = can_swap_sort([1, 6, 3, 4, 5, 2, 7], 7)
+        res4 = can_swap_sort([1, 6, 3, 5, 4, 2, 7], 7)
+        res5 = can_swap_sort([2, 4, 3, 1], 4)
+        res6 = can_swap_sort([1, 2, 4, 3], 4)
+        res7 = can_swap_sort([9, 5, 3, 7, 1], 5)
+
+
+        self.assertTrue(res1)
+        self.assertTrue(res2)
+        self.assertTrue(res3)
+        self.assertTrue(res6)
+
+        self.assertFalse(res4)
+        self.assertFalse(res5)
+        self.assertFalse(res7)
+
 
 if __name__ == '__main__':
     unittest.main()
